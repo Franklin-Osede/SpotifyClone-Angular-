0e99618cda5b1755a411bb7ea99f70fd165c0ae8
Franklin <franklin.op@hotmail.com>
2024-06-28 18:28:09 +0200

    added buttons to reproductions achieved

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent {
+  mockCover: any = {
+    cover: 'https://i.scdn.co/image/ab676160000b27345ca41b0d2352242c7c9d4bc',
+    album: 'Gioli & Assu',
+    name: 'BBE (Oficial)',
+  }
 
 }
