edaf8dd199b16e18989c55628a33a1efe5f318b9
Franklin <franklin.op@hotmail.com>
2024-07-01 19:21:28 +0200

    addded subscription service to tracks

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as dataRaw from '../../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from '@modules/services/track.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.css'
 })
-export class TracksPageComponent implements OnInit {
-  mockTracksList: Array<TrackModel> = [
- 
-  ]
-  constructor(){}
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending: Array<TrackModel> = []
+  tracksRandom: Array<TrackModel> = []
+
+  listObservers$: Array<Subscription> = []
+
+  constructor(private trackService: TrackService){}
 
   ngOnInit(): void {
-    const {data}: any = (dataRaw as any).default
-    this.mockTracksList = data;
+    const observer1$ = this.trackService.dataTracksTrending$
+        .subscribe(response => {
+         this.tracksTrending = response
+        })
    
+      this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u=> u.unsubscribe())
   }
 
 }
