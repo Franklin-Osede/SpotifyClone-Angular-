98a7b72b6cf7c315eda9e4546726ffbf87e74d80
Franklin <franklin.op@hotmail.com>
2024-07-01 12:30:30 +0200

    created directive for img broken

---
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImgBrokenDirective } from './img-broken.directive';
+
+describe('ImgBrokerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImgBrokenDirective();
+    expect(directive).toBeTruthy();
+  });
+});
