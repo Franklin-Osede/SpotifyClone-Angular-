0c63570feeae3797001500fe938fe696f11d4227
Franklin <franklin.op@hotmail.com>
2024-07-03 11:01:37 +0200

    added htpp client module

--- src/app/modules/services/track.service.ts
+++ src/app/modules/services/track.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TrackModel,  } from '@core/models/tracks.model';
-import { Observable,observable, of } from 'rxjs';
-import * as dataRaw from '../../../app/data/tracks.json'
+import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,26 +12,6 @@ export class TrackService {
   dataTracksRandom$: Observable<any> =of([])
 
   constructor() { 
-    const {data} : any = (dataRaw as any).default;
-
-
-    this.dataTracksTrending$ = of(data)
-
-    this.dataTracksRandom$ = new Observable((observer)=>{
-      
-      const trackExample: TrackModel = {
-        _id:9,
-        name: 'Leve',
-        album: 'Cartel de Santa',
-        url: 'http://',
-        cover: 'https://cdns-images.dzcdn.net/images/cover/60007e3d208b5007239b1409244a063e/1900x1900-000000-80-0-0.jpg'
-      }
-      
-
-      setTimeout(()=>{
-        observer.next([trackExample])
-      },3500)
-      
-    })
+   
   }
 }
--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as dataRaw from '../../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.model';
 import { TrackService } from '@modules/services/track.service';
 import { Subscription } from 'rxjs';
@@ -18,22 +17,12 @@ export class TracksPageComponent implements OnInit, OnDestroy {
   constructor(private trackService: TrackService){}
 
   ngOnInit(): void {
-    const observer1$ = this.trackService.dataTracksTrending$
-        .subscribe(response => {
-         this.tracksTrending = response
-         this.tracksRandom = response
-        })
-
-        const observer2$ = this.trackService.dataTracksRandom$
-        .subscribe(response => {
-         this.tracksRandom = [...this.tracksRandom, ...response]
-        })   
+ 
    
-      this.listObservers$ = [observer1$, observer2$]
   }
 
   ngOnDestroy(): void {
-    this.listObservers$.forEach(u=> u.unsubscribe())
+   
   }
 
 }
