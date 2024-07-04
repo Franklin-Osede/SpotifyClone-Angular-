f4c61501cd5e0ee423d529db8fbb5faa86c575ae
Franklin <franklin.op@hotmail.com>
2024-07-04 12:21:25 +0200

    added session guard protected route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,27 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes} from "@angular/router";
-import { sessionGuard } from "@core/guards/session.guard";
+import { RouterModule, Routes } from "@angular/router";
+import { SessionGuard } from "@core/guards/session.guard"; // Corrected import
 import { HomePageComponent } from "@modules/home/pages/home-page/home-page.component";
-
 
 const routes: Routes = [
     {
-        path:'auth',
-        loadChildren: ()=> import(`./modules/auth/auth.module`).then(m => m.AuthModule)
+        path: 'auth',
+        loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
     },
     {
-        path: '',//localhost:4200/
+        path: '', //localhost:4200/
         component: HomePageComponent,
-        loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule),
-        canActivate: [sessionGuard]
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+        canActivate: [SessionGuard] // Corrected usage
     },
     {
-        path:'**',
-        redirectTo:'/tracks'
+        path: '**',
+        redirectTo: '/tracks'
     }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
-
 })
-export class AppRoutingModule{ }
+export class AppRoutingModule { }
--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -1,5 +1,33 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
 
-export const sessionGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionGuard implements CanActivate {
+
+  constructor(private cookieService: CookieService , private router:Router ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkCookieSession();
+  }
+
+  checkCookieSession(): boolean {
+    try {
+      
+     const token: boolean = this.cookieService.check('token')
+     if(!token){
+      this.router.navigate(['/','auth'])
+     } 
+      return token
+      
+    } catch (e) {
+      console.log('Algo sucedió ?? 🔴', e);
+      return false;
+    }
+  }
+}
