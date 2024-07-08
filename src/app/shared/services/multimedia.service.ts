cb77d8243daa60b800542cbd627f5a7bfcc2b39e
Franklin <franklin.op@hotmail.com>
2024-07-08 11:08:58 +0200

    added time remaining and time elapse of songs

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -10,9 +10,12 @@ export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>()
 
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
-  
   public audio!: HTMLAudioElement
-  
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
+
+
+
   constructor() {
     this.audio = new Audio
     this.trackInfo$.subscribe(responseOK => {
@@ -27,9 +30,38 @@ export class MultimediaService {
     this.audio.addEventListener('timeupdate',this.calculateTime,false)
    }
 
-   private calculateTime():void{
+   private calculateTime=()=>{
     console.log('Disparand evento')
-    const {duration, currentTime}=this. audio
+    const {duration, currentTime}=this.audio
+    console.table([duration, currentTime])
+    this.setTimeElapsed(currentTime)
+    this.setTimeRemaining(currentTime, duration)
+   }
+
+   private setTimeElapsed(currentTime: number): void { 
+    let seconds = Math.floor(currentTime % 60)
+    let minutes = Math.floor((currentTime /60) % 60)
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(displayFormat)
+    
+
+   }
+
+   private setTimeRemaining(currentTime:number, duration:number):void{
+    let timeLeft = duration - currentTime;
+    
+    let seconds = Math.floor(timeLeft % 60)
+    let minutes = Math.floor((timeLeft /60) % 60)
+    
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
+
    }
 
  
